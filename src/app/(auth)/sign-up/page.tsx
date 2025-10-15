463e2cf38fa655a50730d266a428c3df4bd99098
shashankhey <shashankhey1710@gmail.com>
2025-10-15 11:27:43 +0530

    Authentication UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view';
+import React from 'react'
+
+const Page = () => {
+console.log("Sign up page");
+
+  return (
+    <SignUpView />
+  )
+}
+
+export default Page;
+
+// http://localhost:3000/sign-up
