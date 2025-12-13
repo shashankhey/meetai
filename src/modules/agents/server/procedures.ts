c69d9c4117cee1bf2a9382f8a8f2690c1a2a4805
shashankhey <shashankhey1710@gmail.com>
2025-12-13 21:08:13 +0530

    bug fix

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,9 +1,9 @@
 import { db } from "@/db";
-import { agents } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema, agentsUpdateSchema } from "../schemas";
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
 import {
   DEFAULT_PAGE,
   DEFAULT_PAGE_SIZE,
@@ -57,9 +57,8 @@ export const agentsRouter = createTRPCRouter({
     .query(async ({ input, ctx }) => {
       const [existingAgent] = await db
         .select({
-          //TODO: change the actual count
-          meetingCount: sql<number>`5`,
           ...getTableColumns(agents),
+          meetingCount: db.$count(meetings, eq(agents.id, meetings.agentId)),
         })
         .from(agents)
         .where(
@@ -89,8 +88,7 @@ export const agentsRouter = createTRPCRouter({
       const { search, page, pageSize } = input;
       const data = await db
         .select({
-          //TODO: change the actual count
-          meetingCount: sql<number>`5`,
+          meetingCount: db.$count(meetings, eq(agents.id, meetings.agentId)),
           ...getTableColumns(agents),
         })
         .from(agents)
--- src/modules/meetings/ui/components/upcoming-state.tsx
+++ src/modules/meetings/ui/components/upcoming-state.tsx
@@ -1,19 +1,13 @@
 import EmptyState from "@/components/empty-state";
 import { Button } from "@/components/ui/button";
-import { BanIcon, VideoIcon } from "lucide-react";
+import { VideoIcon } from "lucide-react";
 import Link from "next/link";
 
 interface Props {
   meetingId: string;
-  onCancelMeeting: () => void;
-  isCancelling: boolean;
 }
 
-export const UpcomingState = ({
-  meetingId,
-  onCancelMeeting,
-  isCancelling,
-}: Props) => {
+export const UpcomingState = ({ meetingId }: Props) => {
   return (
     <div
       className="bg-white rounded-lg px-4 py-5 flex flex-col
@@ -28,16 +22,7 @@ export const UpcomingState = ({
         className="flex flex-col-reverse lg:flex-row lg:justify-center items-center
       gap-2 w-full"
       >
-        <Button
-          variant="secondary"
-          className="w-full lg:w-auto"
-          onClick={onCancelMeeting}
-          disabled={isCancelling}
-        >
-          <BanIcon />
-          Cancel meeting
-        </Button>
-        <Button disabled={isCancelling} asChild className="w-full lg:w-auto">
+        <Button asChild className="w-full lg:w-auto">
           <Link href={`/call/${meetingId}`}>
             <VideoIcon />
             Start meeting
